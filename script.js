e152f45a1a62da9a40f97ba04269ab8550c4e0e1
rhodensingh70-svg <rhodensingh70@gmail.com>
2026-03-06 11:50:48 +0545

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,21 @@ function checkGuess() {
     let userNum = parseInt(input.value);
 
     if (isNaN(userNum)) {
-        result.innerHTML = "Pehle number toh likho! 😂";
+        result.innerHTML = "Oye! Pehle number toh likho! 😂";
         result.style.color = "orange";
-    } else if (userNum == secretNum) {
-        result.innerHTML = "🎉 Mubarak ho! Sahi guess kiya!";
+    } 
+    else if (userNum === secretNum) {
+        result.innerHTML = "🎉 SHABAASH! Ekdam sahi pakde hain!";
         result.style.color = "white";
-        // Naya number set karein agli baar ke liye
+        // Sahi hone par naya number sochne do computer ko
         secretNum = Math.floor(Math.random() * 10) + 1;
-    } else {
-        result.innerHTML = "❌ Galat! Phir se koshish karo.";
+    } 
+    else if (userNum < secretNum) {
+        result.innerHTML = "📉 Thoda BADA number socho! (Too Low)";
+        result.style.color = "yellow";
+    } 
+    else if (userNum > secretNum) {
+        result.innerHTML = "📈 Thoda CHOTA number socho! (Too High)";
         result.style.color = "yellow";
     }
 }
-
