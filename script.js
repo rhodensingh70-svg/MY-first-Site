0acabc613477388ec8ea704ea9b5eaf2d322efbe
rhodensingh70-svg <rhodensingh70@gmail.com>
2026-03-06 12:04:20 +0545

    Update script.js

--- script.js
+++ script.js
@@ -9,21 +9,18 @@ function checkGuess() {
         result.innerHTML = "Oye! Pehle number toh likho! 😂";
         result.style.color = "orange";
     } 
-    else if (userNum === secretNum)
-    document.body.style.backgroundColor = "#f1c40f"; // Gold color for win!
-setTimeout(() => { document.body.style.backgroundColor = "#2ecc71"; }, 1000);{
-        result.innerHTML = "🎉 SHABAASH! Ekdam sahi pakde hain!";
+    else if (userNum == secretNum) {
+        result.innerHTML = "🎉 SHABAASH! Ekdam sahi guess kiya!";
         result.style.color = "white";
-        // Sahi hone par naya number sochne do computer ko
+        // Naya number sochne do
         secretNum = Math.floor(Math.random() * 10) + 1;
     } 
     else if (userNum < secretNum) {
-        result.innerHTML = "📉 Thoda BADA number socho! (Too Low)";
+        result.innerHTML = "📉 Thoda BADA number socho!";
         result.style.color = "yellow";
     } 
-    else if (userNum > secretNum) {
-        result.innerHTML = "📈 Thoda CHOTA number socho! (Too High)";
+    else {
+        result.innerHTML = "📈 Thoda CHOTA number socho!";
         result.style.color = "yellow";
     }
 }
-
