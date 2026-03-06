201aad78eaa1883a5b3c5453423f7dfcebc6c057
rhodensingh70-svg <rhodensingh70@gmail.com>
2026-03-06 11:23:58 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-// Ye pehle wala button ka function hai
-function greetUser() {
-    alert("Hello! Thanks for visiting my website.");
-}
 
-// Ye naya function hai jo form submit hone par chalega
-document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Isse page refresh nahi hoga
-    alert("Shukriya! Aapka message bhej diya gaya hai.");
-});
-
-<div class="game-section">
-    <h3>Game: Guess a Number (1-10)</h3>
-    <input type="number" id="guessInput" placeholder="Enter number">
-    <button onclick="checkGuess()">Guess!</button>
-    <p id="gameResult"></p>
-</div>
 let randomNumber = Math.floor(Math.random() * 10) + 1;
 
 function checkGuess() {
